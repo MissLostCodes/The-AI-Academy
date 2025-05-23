a0a1842965959673f5d88a17ae4a740153c55e2d
Shagun Gupta <shagungupta810@gmail.com>
2025-05-23 10:12:09 +0530

    inngest deployment setup

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,8 +2,10 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import {  CreateNewUser,GenerateCheatSheet, GenerateNotes, GenerateStudyTypeContent, helloWorld } from "../../../inngest/functions";
 // Create an API that serves zero functions
+export const runtime ='edge' ;
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming : 'allow',
   functions: [
     /* your functions will be passed here later! */
     helloWorld ,
